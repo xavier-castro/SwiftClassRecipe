1cf7fc7198d06a68760dfc5c510d0984ee5bbd4d
xavier-castro <xavier_castro@icloud.com>
2019-02-06 15:21:45 -0600

    Initial Commit

---
+++ Convenience Initializer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Convenience Initializer
+//
+//  Created by Xavier Castro on 2/6/19.
+//  Copyright © 2019 Xavier Castro. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
